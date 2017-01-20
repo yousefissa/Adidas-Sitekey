333ff8a13929debe4ed7b10732aaeb70c99e018e
yomo710 <yomo710@gmail.com>
2017-01-20 02:26:31 -0600

    Added AU support.

--- sitekey_twitter.py
+++ sitekey_twitter.py
@@ -9,6 +9,7 @@ from datetime import datetime
 
 US_link = ('http://www.adidas.com/us/ultra-boost-uncaged-shoes/BA9797.html', 'US')
 UK_link = ('http://www.adidas.co.uk/white-mountaineering-nmd-trail-shoes/BA7518.html', 'UK')
+AU_link = ('http://www.adidas.com.au/white-mountaineering-nmd-trail/BA7518.html', 'AU')
 
 # sitekey retrieval
 def get_sitekey(country_link):
@@ -25,14 +26,15 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 def send_tweet(country_link):
-	print('Tweeted sitekey.')
+	api.update_status('Current Adidas {} Sitekey as of: {} is {}'.format(country_link[1], datetime.now(), get_sitekey(country_link)))
+	print('Tweeted {} sitekey.'.format(country_link[1]))
 
 def main():
-	current_sitekey_US, current_sitekey_Uk = get_sitekey(US_link), get_sitekey(UK_link)
-	send_tweet(US_link), send_tweet(UK_link)
-	while (current_sitekey_US == get_sitekey(US_link)) & (current_sitekey_Uk == get_sitekey(UK_link)):
-		sleep(10*60)
+	current_sitekey_US, current_sitekey_Uk, current_sitekey_AU = get_sitekey(US_link), get_sitekey(UK_link), get_sitekey(AU_link)
+	send_tweet(US_link), send_tweet(UK_link), send_tweet(AU_link)
+	while (current_sitekey_US == get_sitekey(US_link)) & (current_sitekey_Uk == get_sitekey(UK_link)) & (current_sitekey_AU == get_sitekey(AU_link)):
 		print('Sleeping 10 minutes.')
+		sleep(10*60)
 	print('Sitekey has changed!')
 	main()
 
